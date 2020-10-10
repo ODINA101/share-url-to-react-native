2b8971228b3409b61753e5f7056b03480496baf2
ODINA101 <iraklisamniashviliii@gmail.com>
2020-10-10 05:39:42 +0400

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {
+  Text,
+  Linking,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+class App extends Component {
+  
+
+  componentDidMount = () => {
+    alert(this.props.url)
+  }
+  render() {
+    return (
+      <View style={{flex:1,backgroundColor:'black'}} >
+      <Text style={{color:'#FFF',fontSize:17}}>this is your text: {this.props.url}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+
+export default App;
---
+++ android/app/src/main/java/com/shareurl/MainActivity.java
@@ -0,0 +1,35 @@
+package com.shareurl;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import android.os.Bundle;
+import android.content.Intent;
+import androidx.annotation.Nullable;
+
+
+public class MainActivity extends ReactActivity {
+  @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Nullable
+            @Override
+            protected Bundle getLaunchOptions() {
+                Intent intent = MainActivity.this.getIntent();
+                Bundle bundle = new Bundle();
+                bundle.putString("url", intent.getStringExtra(Intent.EXTRA_TEXT));
+                return bundle;
+            }
+        };
+    }
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "shareURL";
+  }
+
+
+}
